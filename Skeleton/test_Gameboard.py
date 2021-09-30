b5f9307e8352645a5f2dc072a13c4005f7d00333
Chi Wu <family97109@gmail.com>
2021-09-29 22:18:46 -0400

    modify test case

--- Skeleton/test_Gameboard.py
+++ Skeleton/test_Gameboard.py
@@ -3,81 +3,33 @@ from Gameboard import Gameboard
 
 
 class Test_TestGameboard(unittest.TestCase):
-    def test_move(self):
+    def setUp(self):
+        self.game = Gameboard()
+        self.game.player1 = "red"
+        self.game.player2 = "yellow"
+
+    def test_valid_move(self):
         # 1. Check happy path for correct move
-        game = Gameboard()
-        game.board = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, "red", 0, 0, 0, 0],
-            ["yellow", "yellow", "yellow", 0, 0, 0, 0],
-            ["red", "red", "red", 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p2'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 35
+        self.game.move(0, 'p1')
+        self.assertEqual(self.game.board[5][0], self.game.player1)
+        self.game.move(1, 'p2')
+        self.assertEqual(self.game.board[5][1], self.game.player2)
 
-        # Valid move part 1
-        ANSWER1 = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, "red", 0, 0, 0, 0],
-            ["yellow", "yellow", "yellow", 0, 0, 0, 0],
-            ["red", "red", "red", "yellow", 0, 0, 0]
-        ]
-        game.move(3, 'p2')
-        self.assertEqual(game.board, ANSWER1)
-
-        # Valid move part 2
-        ANSWER2 = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, "red", 0, 0, 0, 0],
-            ["yellow", "yellow", "yellow", "red", 0, 0, 0],
-            ["red", "red", "red", "yellow", 0, 0, 0]
-        ]
-        game.move(3, 'p1')
-        self.assertEqual(game.board, ANSWER2)
-
+    def test_invalid_move(self):
+        # 1. Check happy path for correct move
         # Invalid move: player1 moves before picking a color
-        ANSWER2 = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p1'
-        game.player1 = ""
-        game.player2 = ""
-        game.remaining_moves = 42
+        self.game.player1 = ""
+        self.game.player2 = ""
         self.assertEqual(
-            game.move(3, 'p1'),
+            self.game.move(3, 'p1'),
             (True, "", "Player1 should pick a color first!")
             )
 
     def test_is_current_turn(self):
         # 2. Check if is the current player's turn
-        game = Gameboard()
-        game.board = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, "red", 0, 0, 0, 0],
-            ["yellow", "yellow", "yellow", 0, 0, 0, 0],
-            ["red", "red", "red", 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p2'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 35
-        self.assertEqual(game.move(0, 'p1'), (True, "", "Not your turn"))
-        self.assertEqual(game.move(1, 'p1'), (True, "", "Not your turn"))
+        self.assertEqual(self.game.move(0, 'p2'), (True, "", "Not your turn"))
+        self.game.move(0, 'p1')
+        self.assertEqual(self.game.move(1, 'p1'), (True, "", "Not your turn"))
 
     def test_is_game_over(self):
         # 3. Check if winner was already declared
@@ -89,9 +41,8 @@ class Test_TestGameboard(unittest.TestCase):
 
     def test_is_draw(self):
         # 4. Check if the game is tied
-        game = Gameboard()
         # Case1: The game is over and tied
-        game.board = [
+        self.game.board = [
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
             ["red", "red", "red", "yellow", "yellow", "yellow", "red"],
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
@@ -99,13 +50,11 @@ class Test_TestGameboard(unittest.TestCase):
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
             ["red", "red", "red", "yellow", "yellow", "yellow", "red"]
         ]
-        game.current_turn = 'p1'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 0
-        self.assertEqual(game.move(0, 'p1'), (False, "Draw", None))
+        self.game.remaining_moves = 0
+        self.assertEqual(self.game.move(0, 'p1'), (False, "Draw", None))
+
         # Case2: Last move and the game is tied in the end
-        game.board = [
+        self.game.board = [
             ["yellow", "yellow", "yellow", "red", "red", "yellow", 0],
             ["red", "red", "red", "yellow", "red", "red", "yellow"],
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
@@ -113,13 +62,12 @@ class Test_TestGameboard(unittest.TestCase):
             ["yellow", "yellow", "yellow", "red", "red", "yellow", "red"],
             ["red", "red", "red", "yellow", "red", "yellow", "yellow"]
         ]
-        game.current_turn = 'p2'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 1
-        self.assertEqual(game.move(6, 'p2'), (False, "p2", None))
+        self.game.current_turn = 'p2'
+        self.game.remaining_moves = 1
+        self.assertEqual(self.game.move(6, 'p2'), (False, "p2", None))
+
         # Case3: Last move and player2 wins the game
-        game.board = [
+        self.game.board = [
             ["yellow", "yellow", "yellow", "red", "red", "red", 0],
             ["red", "red", "red", "yellow", "yellow", "yellow", "red"],
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
@@ -127,65 +75,43 @@ class Test_TestGameboard(unittest.TestCase):
             ["yellow", "yellow", "yellow", "red", "red", "red", "yellow"],
             ["red", "red", "red", "yellow", "yellow", "yellow", "red"]
         ]
-        game.current_turn = 'p2'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 1
-        self.assertEqual(game.move(6, 'p2'), (False, "Draw", None))
+        self.game.current_turn = 'p2'
+        self.game.remaining_moves = 1
+        self.assertEqual(self.game.move(6, 'p2'), (False, "Draw", None))
 
     def test_is_column_full(self):
         # 5. Check if the current column is filled
-        game = Gameboard()
-        game.board = [
-            ["yellow", 0, 0, 0, 0, 0, 0],
-            ["red", 0, 0, 0, 0, 0, 0],
-            ["yellow", 0, 0, 0, 0, 0, 0],
-            ["red", 0, 0, 0, 0, 0, 0],
-            ["yellow", 0, 0, 0, 0, 0, 0],
-            ["red", 0, 0, 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p1'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 36
-        self.assertEqual(game.move(0, 'p1'), (True, "", "The column is full"))
+        for i in range(5, 0, -2):
+            self.game.board[i][0] = self.game.player1
+        for i in range(4, -1, -2):
+            self.game.board[i][0] = self.game.player2
+        self.game.remaining_moves = 36
+        self.assertEqual(
+            self.game.move(0, 'p1'),
+            (True, "", "The column is full")
+        )
 
-    def test_check_winner(self):
-        # 6. Check happy path for winning move in each of
-        # horizontal, vertical, positive/negative slope diagonal
-        game = Gameboard()
-        # Horizontal
-        game.board = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            ["yellow", "yellow", "yellow", 0, 0, 0, 0],
-            ["red", "red", "red", 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p1'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 36
-        game.move(3, 'p1')
-        self.assertEqual(game.game_result, 'p1')
-        # Vertical
-        game.board = [
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            ["red", "yellow", 0, 0, 0, 0, 0],
-            ["red", "yellow", 0, 0, 0, 0, 0],
-            ["red", "yellow", 0, 0, 0, 0, 0]
-        ]
-        game.current_turn = 'p1'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 36
-        game.move(0, 'p1')
-        self.assertEqual(game.game_result, 'p1')
-        # positive slope diagonal
-        game.board = [
+    def test_winning_move_horizontal(self):
+        # Checks if there is a winning move in horizontal direction
+        for i in range(3):
+            self.game.board[5][i] = self.game.player1
+            self.game.board[4][i] = self.game.player2
+        self.game.remaining_moves = 36
+        self.game.move(3, 'p1')
+        self.assertEqual(self.game.game_result, 'p1')
+
+    def test_winning_move_vertical(self):
+        # Checks if there is a winning move in vertical direction
+        for i in range(3):
+            self.game.board[5-i][0] = self.game.player1
+            self.game.board[5-i][1] = self.game.player2
+        self.game.remaining_moves = 36
+        self.game.move(0, 'p1')
+        self.assertEqual(self.game.game_result, 'p1')
+
+    def test_winning_move_positive_slope_diagonal(self):
+        # Checks if there is a winning move in positive diagonal direction
+        self.game.board = [
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
@@ -193,27 +119,23 @@ class Test_TestGameboard(unittest.TestCase):
             [0, "red", "yellow", "red", "yellow", 0, 0],
             ["red", "yellow", "yellow", "red", 0, 0, 0]
         ]
-        game.current_turn = 'p1'
-        game.player1 = "red"
-        game.player2 = "yellow"
-        game.remaining_moves = 32
-        game.move(3, 'p1')
-        self.assertEqual(game.game_result, 'p1')
-        # negative slope diagonal
-        game.board = [
+        self.game.remaining_moves = 32
+        self.game.move(3, 'p1')
+        self.assertEqual(self.game.game_result, 'p1')
+
+    def test_winning_move_negative_slope_diagonal(self):
+        # Checks if there is a winning move in negative diagonal direction
+        self.game.board = [
             [0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0],
-            ["red", 0, 0, 0, 0, 0, 0],
+            ["red", "red", 0, 0, 0, 0, 0],
             ["yellow", "red", 0, 0, 0, 0, 0],
             ["red", "yellow", "red", 0, 0, 0, 0],
             ["yellow", "yellow", "yellow", 0, 0, 0, 0]
         ]
-        game.current_turn = 'p2'
-        game.player1 = "yellow"
-        game.player2 = "red"
-        game.remaining_moves = 33
-        game.move(3, 'p2')
-        self.assertEqual(game.game_result, 'p2')
+        self.game.remaining_moves = 32
+        self.game.move(3, 'p1')
+        self.assertEqual(self.game.game_result, 'p1')
 
 
 # if __name__ == '__main__':
